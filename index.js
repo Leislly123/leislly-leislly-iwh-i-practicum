47cec8bc67a8151cb023468097fb426d4f0b9f8b
DanielCileaz <danielanos.vold@gmail.com>
2025-05-21 14:01:57 +0700

    second update

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
+require('dotenv').config(); // ← ini wajib di baris pertama
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
+// Ambil token dari .env
+const HUB_API_KEY = process.env.HUBSPOT_API_KEY;
+console.log("Token:", HUB_API_KEY); // Debug token
+
+// ID Custom Object kamu (misalnya: Panda)
+const objectTypeId = '2-168780239';
+
+const headers = {
+    "Authorization": `Bearer ${HUB_API_KEY}`,
+    "Content-Type": "application/json",
+};
+
 app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// ROUTE 1 - Homepage: tampilkan data custom object
+app.get("/", async (req, res) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectTypeId}?properties=name,bio,species`;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        const { data } = await axios.get(url, { headers });
+        console.log("Fetched pandas:", data.results); // Debug isi data
+        res.render("homepage", {
+            title: "Pandas List | HubSpot Custom Object",
+            pandas: data.results,
+        });
     } catch (error) {
-        console.error(error);
+        console.error('Error fetching Panda data:', error.response?.data || error.message);
+        res.status(500).send('Error fetching Panda data');
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+// ROUTE 2 - Halaman form input custom object
+app.get("/update-cobj", (req, res) => {
+    res.render("updates", {
+        title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+    });
+});
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+// ROUTE 3 - Submit form untuk menambah custom object
+app.post("/update-cobj", async (req, res) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectTypeId}`;
+    const body = {
+        properties: {
+            name: req.body.name,
+            bio: req.body.bio,
+            species: req.body.species,
+        },
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    console.log("Sending data to HubSpot:", body); // Debug body yang dikirim
+
+    try {
+        await axios.post(url, body, { headers });
+        res.redirect("/");
+    } catch (err) {
+        console.error('Error creating Panda:', err.response?.data || err.message);
+        res.status(500).send('Error creating Panda');
     }
-
 });
-*/
 
-
-// * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
